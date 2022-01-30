4c93e49836183d3fc3639e6c8cb069c2eed4cc24
Jeff McNeill <jeff@mcneill.io>
2022-01-30 17:26:07 +0700

    add some cloudflare suff

--- packages/site/svelte.config.js
+++ packages/site/svelte.config.js
@@ -15,4 +15,15 @@ const config = {
 	}
 };
 
+++ import adapter from '@sveltejs/adapter-static';
+++
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  kit: {
+++  adapter: adapter(),
+    // ... truncated ...
+    target: '#svelte'  
+  }
+};
+
 export default config;
